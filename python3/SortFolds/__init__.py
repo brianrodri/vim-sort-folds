a117003f0b3b31f194d0f7103c2e622a4703a839
Brian Rodriguez <brian@brianrodri.com>
2019-05-06 03:47:16 -0400

    get sorting mildly working

--- python3/SortFolds/__init__.py
+++ python3/SortFolds/__init__.py
@@ -4,6 +4,7 @@
 
 Maintainer:	Brian Rodriguez
 """
+import contextlib
 import itertools
 import operator
 import vim
@@ -28,8 +29,10 @@ class Fold():
         if start > end:
             raise IndexError(f'start must not be greater than end, but got '
                              f'start={start} and end={end}')
-        self._start = start - 1
-        self._end = end - 1
+        self._start, self._end = start, end
+
+    def __iter__(self):
+        yield from vim.current.buffer[self._start:self._end]
 
     def __len__(self):
         return self._end - self._start
@@ -46,23 +49,38 @@ def sort_folds(key_line_number=0):
 
 def get_folds():
     """Yields all folds in the currently selected range."""
-    fold_starts, fold_ends = itertools.tee(get_fold_starting_positions())
-    next(fold_ends)  # Drop the first fold position.
-    yield from (Fold(start, end) for start, end in zip(fold_starts, fold_ends))
+    start, *remaining_starts = get_fold_starting_positions()
+    for end in remaining_starts:
+        yield Fold(start, end)
+        start = end
 
 
 def get_fold_starting_positions():
     """Yields the starting line numbers of all folds in the current range."""
+    range_start, range_end = vim.current.range.start, vim.current.range.end - 1
+    with restore_cursor():
+        fold_start = range_start
+        while fold_start <= range_end:
+            yield fold_start
+            vim.command('normal! zj')
+            next_fold_start = get_cursor_line_number() - 1
+            if fold_start < next_fold_start:
+                fold_start = next_fold_start
+            else:
+                vim.command('normal! ]z')
+                yield get_cursor_line_number()
+                return
+        vim.command('normal! zk')
+        yield get_cursor_line_number()
+
+
+@contextlib.contextmanager
+def restore_cursor():
     initial_cursor = vim.current.window.cursor
-    range_start, range_end = vim.current.range.start, vim.current.range.end + 1
-    fold_start = range_start
-    while fold_start < range_end:
-        yield fold_start
-        vim.command('normal! zj')
-        next_fold_start = int(vim.eval('line(".")'))
-        if fold_start < next_fold_start:
-            fold_start = next_fold_start
-        else:
-            break
-    yield range_end
-    vim.current.window.cursor = initial_cursor
+    try:
+        yield
+    finally:
+        vim.current.window.cursor = initial_cursor
+
+def get_cursor_line_number():
+    return int(vim.eval('line(".")'))
