caade89fd70762729f9eaaa5dcc4c93068a359cf
Brian Rodriguez <brian@brianrodri.com>
2019-05-07 08:21:11 -0400

    fix calls to sort_fold

--- autoload/sortfolds.vim
+++ autoload/sortfolds.vim
@@ -7,6 +7,6 @@ py3 import vim
 py3 import sort_folds
 
 function! sortfolds#SortFolds(...) range
-  execute a:firstline . ',' . a:lastline . ' py3 sort_folds.sort_folds(' .
-      \ get(a:, 0, 0) . ')'
+  silent execute a:firstline . ',' . a:lastline .
+      \ ' py3 sort_folds.sort_folds(' . get(a:, 0, 0) . ')'
 endfunction
---
+++ python3/sort_folds/fold.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""Provides the helper class VimFold for working with folds in vim."""
+import collections
+import vim
+
+
+class VimFold(collections.abc.MutableSequence):
+    """An interface for working with a vim fold.
+
+    Folds behave like sequences when subscripted using another sequence:
+        >>> fold = VimFold(start_line_num=1, end_line_num=3)
+        >>> sequence = ['line 1', 'line 2', 'line 3']
+        >>> fold[sequence]
+        ['line 1', 'line 2']
+        >>> fold[sequence] = ['line A', 'line B', 'line C']
+        >>> sequence
+        ['line A', 'line B', 'line C', 'line 3']
+        >>> del fold[sequence]
+        >>> sequence
+        ['line C', 'line 2', 'line 3']
+
+    The rest of VimFold's interface acts directly on vim's current buffer.
+
+    Attributes:
+        start: int. The buffer index at which the fold starts (inclusive).
+        end: int. The buffer index at which the fold ends (exclusive).
+    """
+    def __init__(self, start_line_num, end_line_num):
+        """Initializes a new VimFold from the given pair of line numbers.
+
+        Args:
+            start_line_num: int. Line number at which self starts (inclusive).
+            end_line_num: int. Line number at which self ends (exclusive).
+        """
+        self.start = start_line_num - 1
+        self.end = end_line_num - 1
+
+    def get(self, index):
+        """Get line from vim's current buffer at index, offset by self's start.
+
+        Args:
+            index: int.
+
+        Returns:
+            str. The corresponding line in vim's current buffer, offset by the
+                self's position.
+        """
+        return vim.current.buffer[self.start + index]
+
+    def insert(self, index, line):
+        """Insert line to vim's current buffer at index, offset by self's start.
+
+        Args:
+            index: int.
+            line: str.
+        """
+        vim.current.buffer.insert(self.start + pos, item)
+
+    def __iter__(self):
+        """Iterates through the lines of self's fold in vim's current buffer."""
+        return (self.get(i) for i in range(self.start, self.end))
+
+    def __len__(self):
+        """Returns the number of lines that self spans."""
+        return self.end - self.start
+
+    def __getitem__(self, sequence):
+        return sequence[self.start:self.end]
+
+    def __setitem__(self, mutable_sequence, item):
+        mutable_sequence[self.start:self.end] = item
+
+    def __delitem__(self, mutable_sequence):
+        del mutable_sequence[self.start:self.end]
