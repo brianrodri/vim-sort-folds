47fdfcd6ba7bdf29cc46eac36186248e60a5bf6b
Brian Rodriguez <brian@brianrodri.com>
2019-05-07 10:58:48 -0400

    readability improvements

--- python3/sort_folds/__init__.py
+++ python3/sort_folds/__init__.py
@@ -17,15 +17,13 @@ def sort_folds(line_index_key=0):
         line_index_key: int. Index of line to use as a fold's comparison key.
     """
     folds = [fold.VimFold(fstart, fend) for fstart, fend in cursor.walk_folds()]
-    if len(folds) < 2:
-        return
-    sorted_folds = sorted(folds, key=make_fold_key(line_index_key))
-
-    initial_buffer = vim.current.buffer[:]
-    safe_folds_to_swap = reversed(list(zip(folds, sorted_folds)))
-    for old_fold, new_fold in safe_folds_to_swap:
-        old_fold[vim.current.buffer] = new_fold[initial_buffer]
-    present_result()
+    if len(folds) > 1:
+        initial_buffer = vim.current.buffer[:]
+        sorted_folds = sorted(folds, key=make_fold_key(line_index_key))
+        safe_folds_to_swap = reversed(list(zip(folds, sorted_folds)))
+        for old_fold, new_fold in safe_folds_to_swap:
+            old_fold[vim.current.buffer] = new_fold[initial_buffer]
+        present_result()
 
 
 def make_fold_key(line_index_key):
