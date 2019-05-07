fcef79eccaf07707dd69a76b27227686ce53929e
Brian Rodriguez <brian@brianrodri.com>
2019-05-07 10:17:42 -0400

    factor out key function creator

--- python3/sort_folds/__init__.py
+++ python3/sort_folds/__init__.py
@@ -10,24 +10,45 @@ __all__ = ['sort_folds']
 __version__ = '0.3.0'
 
 
-def sort_folds(key_index=0):
+def sort_folds(key_line_index=0):
     """Sorts the folds intersecting vim's current range.
 
     Args:
-        key_index: int. Which line index to use as the folds' comparison key.
+        key_line_index: int. The line index to use as the folds' comparison key.
     """
     with cursor.CursorRestorer():
         initial_folds = [fold.VimFold(*rng) for rng in cursor.walk_over_folds()]
     if len(initial_folds) < 2:
         return
-    sorted_folds = sorted(initial_folds, key=lambda f: f.get(key_index).lower())
+    sorted_folds = sorted(initial_folds, key=make_fold_key(key_line_index))
 
     initial_buffer = vim.current.buffer[:]
     safe_folds_to_swap = reversed(list(zip(initial_folds, sorted_folds)))
     for old_fold, new_fold in safe_folds_to_swap:
         old_fold[vim.current.buffer] = new_fold[initial_buffer]
-    with cursor.CursorRestorer():
-        present_result()
+    present_result()
+
+
+def make_fold_key(key_line_index):
+    """Returns a function which will create keys for comparing VimFolds.
+
+    Args:
+        key_line_index: int. The line index to use as the folds' comparison key.
+
+    Returns:
+        callable(VimFold) -> Comparable.
+    """
+    def fold_key(fold):
+        """Returns a key for comparing VimFold objects during sort.
+
+        Args:
+            fold: VimFold.
+
+        Returns:
+            Comparable.
+        """
+        return fold.get(key_line_index).lower()
+    return fold_key
 
 
 def present_result():
