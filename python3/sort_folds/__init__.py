0760170aa8579e21055551e22afdffcb30ee6112
Brian Rodriguez <brian@brianrodri.com>
2019-05-10 10:44:41 -0400

    cut down on copies made from the buffer

--- python3/sort_folds/__init__.py
+++ python3/sort_folds/__init__.py
@@ -17,9 +17,10 @@ def sort_folds(line_index_key=0):
         folds = [fold.VimFold(*line_nums) for line_nums in cursor.walk_folds()]
     if len(folds) > 1:
         sorted_folds = sorted(folds, key=lambda f: f[line_index_key].lower())
-        initial_buffer = vim.current.buffer[:]
-        for old_fold, new_fold in reversed(list(zip(folds, sorted_folds))):
-            old_fold[:] = new_fold[initial_buffer]
+        sorted_fold_lines = (fold[:] for fold in sorted_folds)
+        safe_assignment_order = reversed(list(zip(folds, sorted_fold_lines)))
+        for out_of_order_fold, in_order_fold_lines in safe_assignment_order:
+            out_of_order_fold[:] = in_order_fold_lines
         present_result()
 
 
